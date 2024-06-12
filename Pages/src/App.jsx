3f5b2f6740ef1912e703bc26cfd98748eb247f21
Abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-06-12 11:21:06 +0530

    basic format

---
+++ Pages/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-gray-100">
+        <h1 className="text-center my-20 py-20">HERE WE GO!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
