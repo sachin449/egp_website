f02b42d1cfcc84d4c2e859e550d57e38543f3fa9
Abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-06-14 18:39:54 +0530

    yo

--- Pages/src/Components/Market.jsx
+++ Pages/src/Components/Market.jsx
@@ -5,7 +5,7 @@ export  const Market = () => {
     <div className="w-full h-screen relative">
       {/* Video Background */}
       <video className="absolute top-0 left-0 w-full h-full object-cover" autoPlay loop muted>
-        <source src="path-to-your-video-file.mp4" type="video/mp4" />
+        <source src="realestatevideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
@@ -15,22 +15,39 @@ export  const Market = () => {
       {/* Heading and Subheading */}
       <div className="relative container mx-auto flex flex-col justify-end h-full px-5 py-10 text-white">
         <div className="mb-20"> {/* Adjusted margin-bottom for spacing */}
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4">Building a better future</h1>
-          <h2 className="text-lg sm:text-xl md:text-2xl">
-            Creating buildings and urban areas where people, business, and nature thrive.
+          <h1 className=" text-3xl sm:text-5xl md:text-5xl  font-medium-bold mb-6">Infrastructure & Real Estate</h1>
+          <h2 className="w-[800px] text-lg sm:text-xl md:text-2xl md:text-medium-bold">
+          We have been providing services to Client in sectors of Infrastructure & Real Estate,
+           Manufacturing, Agri-Business & Forestry, Retail & Hospitality, Financial Institution &
+            Funds and Health & Education.
           </h2>
         </div>
       </div>
 
       {/* Content Below Video */}
-      <div className="bg-white py-10 px-5">
-        <div className="container mx-auto">
-          <h2 className="text-3xl sm:text-4xl font-bold text-blue-600 mb-6">
-            Designing sustainable, resilient, and attractive buildings and spaces for life to flourish
-          </h2>
-          <p className="text-lg sm:text-xl text-gray-700">
-            With the global real estate market projected to grow by 5% annually, the question of how to create thriving urban neighbourhoods and buildings is paramount. To be attractive, real estate must meet tenant and end user demands for sustainability, social interaction, and a good lifestyle. At the same time, the long term viability of developments depends on them being flexible to changing needs over time and resilient to shocks such as extreme weather and pandemics.
+      <div className="bg-white pr-40  py-16 ">
+        <div className="container w-[900px] pr-[200px] mx-auto">
+          
+          <h4 className="text-4xl sm:text-4xl font-medium-bold text-blue-600 mb-6">
+            Hydropower in Nepal
+          </h4>
+          <p className="my-12  sm:text-xl text-gray-700 text-balance">
+          
+          EGP has undertaken development of Management Plan for a 50 MW hydropower project in Nepal. Our team has provided
+           services related to development of EHS management plan and supported the Project Company with advice on
+            managing risks related construction phase of the Project.
           </p>
+          <h4 className="text-4xl sm:text-4xl font-medium-bold text-blue-600 mb-6">
+            Solar Power Project in India
+          </h4>
+          <p className="my-12  sm:text-xl text-gray-700 text-balance">
+          
+            Members of our team was involved in developing an Environment and Social Impact Assessment (ESIA) for a 250 MW
+            Solar Power Project in India. The project entailed assessing the risks and impacts of the project and
+            developing appropriate mitigation measures as part of the Environment and Social Management Plan. 
+            Critical Habitat Assessment and Advisory was also carried out for the project.
+          </p>
+          
         </div>
       </div>
     </div>
--- Pages/src/Components/Services.jsx
+++ Pages/src/Components/Services.jsx
@@ -5,13 +5,13 @@ const services = [
     id: 1,
     backgroundImage: 'url(sec8.jpg)',
     heading: 'ESG Risk Advisory',
-    subheading: 'The risks and impacts emanating from environment, social and governance(ESG)...',
+    subheading: 'The risks and impacts emanating from environment, social and governance...',
     link: '/page1',
   },
   {
     id: 2,
     backgroundImage: 'url(sec7.jpg)',
-    heading: 'Mergers & Acquisitions',
+    heading: 'Merger & Acquisition',
     subheading: 'EGP offers services to Clients in Mergers & Acquisitions (M&A) through ...',
     link: '/page2',
   },
@@ -51,12 +51,12 @@ const Services = () => {
           onMouseEnter={() => setHoveredService(index)}
           onMouseLeave={() => setHoveredService(0)}
         >
-          <div className="relative flex flex-col items-center justify-end w-full h-full p-12 text-white bg-black bg-opacity-10  hover:bg-opacity-30 border-2 border-transparent transition duration-500 ease-in-out border-s-gray-900 ">
-            <div className="absolute bottom-[50px] flex flex-col items-start transition-all duration-1000 ease-in-out left-[30px]">
-              <h2 className={`text-2xl sm:text-2xl md:text-3xl  ${hoveredService === index ? 'mb-8' : 'mb-2'}`}>
+          <div className="relative flex flex-col items-center justify-end w-full h-full p-4 text-white bg-black bg-opacity-10 hover:bg-opacity-30 border-2 border-transparent transition duration-500 ease-in-out border-s-gray-900 ">
+            <div className=" relative flex flex-col items-start transition-all duration-1000 ease-in-out">
+              <h2 className={` text-balance text-2xl sm:text-2xl md:text-3xl transition-all duration-700 ease-in-out ${hoveredService === index ? 'mb-2 translate-y-[-50px]' : 'mb-0 translate-y-[10px]'}`}>
                 {service.heading}
               </h2>
-              <p className={`text-lg sm:text-base transition-opacity duration-500 ease-in-out ${hoveredService === index ? 'opacity-100' : 'opacity-0'}`}>
+              <p className={`  absolute  w-64  text-lg sm:text-base transition-opacity duration-700 ease-in-out ${hoveredService === index ? 'opacity-100' : 'opacity-0'}`}>
                 {service.subheading}
               </p>
               <button
@@ -66,16 +66,12 @@ const Services = () => {
                 }}
               >
                 <span
-                  className={`transition-all duration-500 ease-in-out ${
-                    hoveredService === index? 'opacity-100 translate-x-[-10px]' : 'opacity-0 translate-x-2'
-                  }`}
+                  className={` mt-5 relative transition-all duration-1000 ease-in-out ${hoveredService === index ? 'opacity-100 translate-x-[-10px]' : 'opacity-0 translate-x-2'}`}
                 >
                   Discover
                 </span>
                 <span
-                  className={`ml-2 transition-all duration-500 ease-in-out ${
-                    hoveredService === index? 'opacity-100 translate-x-[-10px]' : 'opacity-100 -translate-x-2'
-                  }`}
+                  className={`mt-5 absolute ml-2 transition-all duration-1000 ease-in-out ${hoveredService === index ? 'opacity-100 translate-x-[50px]' : 'opacity-100 -translate-x-2'}`}
                 >
                   →
                 </span>
